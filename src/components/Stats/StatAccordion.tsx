04d187de702c902d7ee07570ffdc49f64cc1f430
virtualp1 <zininnikita309@gmail.com>
2025-07-03 15:05:27 +0400

    Refactor StatAccordion: replace custom Accordion with MUI Accordion, enhance styling for improved appearance and consistency

--- src/components/Stats/StatAccordion.tsx
+++ src/components/Stats/StatAccordion.tsx
@@ -1,9 +1,10 @@
 import { type FC } from 'react'
+import MuiAccordion, { type AccordionProps } from '@mui/material/Accordion'
 import {
-  Accordion,
   AccordionSummary,
   AccordionDetails,
   Typography,
+  styled,
 } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import StatTable from './StatTable'
@@ -13,11 +14,23 @@ interface Props {
   section: StatSection
 }
 
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&::before': {
+    display: 'none',
+  },
+}))
+
 const StatAccordion: FC<Props> = ({ section }) => {
   const { title, icon: Icon, data, type } = section
 
   return (
-    <Accordion color="dark" disableGutters>
+    <Accordion>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <Icon sx={{ mr: 1 }} />
         <Typography>{title}</Typography>
